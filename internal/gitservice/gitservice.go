1fb3e565164b918ed10ad87d082960fb1cbdadc5
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2024-02-05 19:13:50 +0000

    feat: add internal packages

---
+++ internal/gitservice/gitservice.go
@@ -0,0 +1,55 @@
+package gitservice
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/imroc/req/v3"
+)
+
+type ErrorMessage struct {
+	Message string `json:"message"`
+}
+
+type UserInfo struct {
+	Name string `json:"name"`
+	Blog string `json:"blog"`
+}
+
+var client = req.C().
+	SetTimeout(5 * time.Second)
+
+func Get() {
+	var userInfo UserInfo
+	var errMsg ErrorMessage
+
+	resp, err := client.R().
+		SetPathParam("username", "imroc").
+		SetSuccessResult(&userInfo).
+		SetErrorResult(&errMsg).
+		EnableDump().
+		Get("https://api.github.com/users/{username}")
+
+	if err != nil { // Error handling.
+		log.Println("error:", err)
+		log.Println("raw content:")
+		log.Println(resp.Dump()) // Record raw content when error occurs.
+		return
+	}
+
+	if resp.IsErrorState() { // Status code >= 400.
+		fmt.Println(errMsg.Message) // Record error message returned.
+		return
+	}
+
+	if resp.IsSuccessState() { // Status code is between 200 and 299.
+		fmt.Printf("%s (%s)\n", userInfo.Name, userInfo.Blog)
+		return
+	}
+
+	// Unknown status code.
+	log.Println("unknown status", resp.Status)
+	log.Println("raw content:")
+	log.Println(resp.Dump()) // Record raw content when server returned unknown status code.
+}
