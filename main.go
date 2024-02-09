971deac9164474cd3455a5ca872c34f86a33d4f1
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2024-02-09 19:39:27 -0300

    feat: package from args

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/alexflint/go-arg"
 	"github.com/eduhds/gspm/internal/gitservice"
 	"github.com/eduhds/gspm/internal/tui"
@@ -20,63 +23,74 @@ var args struct {
 func main() {
 	arg.MustParse(&args)
 
-	tui.ShowMessage("Welcome to GSPM! " + args.Command + " " + args.Value)
+	if args.Command == "add" {
+		packageInfo := strings.Split(args.Value, "@")
 
-	var gsPackage GSPackage
-	gsPackage.Name = "eduhds/logduto"
-	gsPackage.Tag = "latest"
+		var gsPackage GSPackage
+		gsPackage.Name = packageInfo[0]
 
-	stopReleasesSpn := tui.ShowSpinner("Fetching releases...")
-
-	releases, err := gitservice.GetGitHubReleases(gsPackage.Name)
-
-	if err != nil {
-		stopReleasesSpn("fail")
-		panic(err)
-	}
-
-	stopReleasesSpn("success")
-
-	var tag string
-	var tagOptions []string
-	var assets = make(map[string][]gitservice.GSGitHubReleaseAsset)
-	var assetOptions []string
-
-	for _, release := range releases {
-		if gsPackage.Tag != "" && release.TagName == gsPackage.Tag {
-			tag = release.TagName
-			break
+		if len(strings.Split(gsPackage.Name, "/")) != 2 {
+			panic("Invalid package name")
 		}
-		tagOptions = append(tagOptions, release.TagName)
-		assets[release.TagName] = release.Assets
-	}
 
-	if tag == "" {
-		tag = tui.ShowOptions("Select a tag", tagOptions)
-	}
-
-	gsPackage.Tag = tag
-
-	for _, asset := range assets[tag] {
-		assetOptions = append(assetOptions, asset.Name)
-	}
-
-	assetName := tui.ShowOptions("Select an asset", assetOptions)
-
-	stopAssetSpn := tui.ShowSpinner("Downloading asset...")
-
-	var res bool
-
-	for _, asset := range assets[tag] {
-		if asset.Name == assetName {
-			res = gitservice.GetGitHubReleaseAsset(asset)
-			break
+		if len(packageInfo) > 1 {
+			gsPackage.Tag = packageInfo[1]
 		}
-	}
 
-	if res {
-		stopAssetSpn("success")
+		stopReleasesSpn := tui.ShowSpinner(fmt.Sprintf("Fetching %s releases...", gsPackage.Name))
+
+		releases, err := gitservice.GetGitHubReleases(gsPackage.Name)
+
+		if err != nil {
+			stopReleasesSpn("fail")
+			panic(err)
+		}
+
+		stopReleasesSpn("success")
+
+		var tag string
+		var tagOptions []string
+		var assets = make(map[string][]gitservice.GSGitHubReleaseAsset)
+		var assetOptions []string
+
+		for _, release := range releases {
+			if gsPackage.Tag != "" && release.TagName == gsPackage.Tag {
+				tag = release.TagName
+				break
+			}
+			tagOptions = append(tagOptions, release.TagName)
+			assets[release.TagName] = release.Assets
+		}
+
+		if tag == "" {
+			tag = tui.ShowOptions("Select a tag", tagOptions)
+		}
+
+		gsPackage.Tag = tag
+
+		for _, asset := range assets[tag] {
+			assetOptions = append(assetOptions, asset.Name)
+		}
+
+		assetName := tui.ShowOptions("Select an asset", assetOptions)
+
+		stopAssetSpn := tui.ShowSpinner(fmt.Sprintf("Downloading %s...", assetName))
+
+		var res bool
+
+		for _, asset := range assets[tag] {
+			if asset.Name == assetName {
+				res = gitservice.GetGitHubReleaseAsset(asset)
+				break
+			}
+		}
+
+		if res {
+			stopAssetSpn("success")
+		} else {
+			stopAssetSpn("fail")
+		}
 	} else {
-		stopAssetSpn("fail")
+		panic("Unknown command: " + args.Command)
 	}
 }
