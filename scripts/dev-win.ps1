6769f94268a83752ee1c8a50e43d1da8c283dbce
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2024-12-27 09:36:19 -0300

    chore: remove old scripts

--- scripts/dev-win.ps1
+++
@@ -1,5 +0,0 @@
-go run . add go-task/task
-
-Expand-Archive -LiteralPath $env:USERPROFILE\Downloads\task_windows_amd64.zip -DestinationPath $env:USERPROFILE\Downloads\task
-
-copy $env:USERPROFILE\Downloads\task\task.exe .
