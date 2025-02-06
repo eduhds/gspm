be5e1ad51f4c0cf3d02326a5926f474fa32847b8
eduardohds.dev <eduardohds.dev@gmail.com>
2025-02-06 23:26:37 +0000

    fix: remove references to update command

--- types.go
+++ types.go
@@ -20,7 +20,7 @@ type args struct {
 	ConfigDir   string   `arg:"env:GSPM_CONFIG_DIR"`
 	ShellCommand string   `arg:"env:GSPM_SHELL_COMMAND"`
 	GitHubToken string   `arg:"env:GSPM_GITHUB_TOKEN"`
-	Command     string   `arg:"positional" help:"Command to run. Must be add, remove, update, install, edit, info or list."`
+	Command     string   `arg:"positional" help:"Command to run. Must be add, remove, install, edit, info or list."`
 	Repos       []string `arg:"positional" help:"Repos from Git Services (GitHub supported only for now). Format: username/repository"`
 	Scripts     []string `arg:"-s,--script,separate" help:"Script to run after download a asset. Use {{ASSET}} to reference the asset path."`
 }
